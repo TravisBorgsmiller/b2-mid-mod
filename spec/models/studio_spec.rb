2f8ab251a10532762752b2637071b043ae4af8ef
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-14 09:41:43 -0700

    add studio spec

--- spec/models/studio_spec.rb
+++ spec/models/studio_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe Studio, type: :model do
   describe 'validations' do
     it {should validate_prescence_of :name}
   end
+  describe 'relationships' do
+    it {should have_many :movies}
+  end
 end
