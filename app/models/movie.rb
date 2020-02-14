94da0c8ac5e56fbdd84ab7dbcf59283d8fab9fcb
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-14 09:44:51 -0700

    update movie validations through join table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   validate_prescence_of :name, :creation_year, :genre
-  has_many actors, through :actor_movies
+  has_many :actor_movies
+  has_many :actors, through: :actor_movies
   belongs_to :studio
 end
